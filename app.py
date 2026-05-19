c9ec922b89087b2488d1572d97ee53cc2813f603
Ayesha Mazhar <ayeshamazhar2885@gmail.com>
2026-05-19 20:01:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
-name= input("Enter your name: ")
-age= input("Enter your age: ")
-print("Hello " + name + "! You are " + age + " years old.") 
-#Simple condition
-if age >= "18":
-    print("You are an adult.")
-else:
-    print("You are a teenager.")
+
     
